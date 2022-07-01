af20d2642f609278eecb828ddb207586d6617dce
lengyibai <1329670984@qq.com>
2022-07-01 11:47:14 +0800

    增加装备页内容主体

---
+++ src/views/Equip/childComps/EquipList/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="EquipList">
+    <EquipListPrimary :equipList="equipList[0]" />
+    <EquipListMedium :equipList="equipList[1]" />
+    <EquipListSenior :equipList="equipList[2]" />
+  </div>
+</template>
+<script>
+//#####··········子组件··········#####//
+import EquipListPrimary from "./childComps/EquipListPrimary"; //初级装备
+import EquipListMedium from "./childComps/EquipListMedium"; //中级装备
+import EquipListSenior from "./childComps/EquipListSenior"; //高级装备
+export default {
+  name: "EquipList",
+  props: {
+    equipList: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+  components: { EquipListPrimary, EquipListMedium, EquipListSenior },
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.EquipList {
+  position: relative;
+  width: 75%;
+  height: 100%;
+  display: flex;
+  margin-right: 1px;
+}
+</style>
