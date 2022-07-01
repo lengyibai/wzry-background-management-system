65234900adb76958aa656c1473c469bf8c72be4e
lengyibai <1329670984@qq.com>
2022-07-01 11:46:09 +0800

    增加初级装备列

---
+++ src/views/Equip/childComps/EquipList/childComps/EquipListPrimary/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="EquipListPrimary">
+    <EquipCard v-for="(item, index) in equipList" :equip="item" :key="index" />
+  </div>
+</template>
+<script>
+//#####··········子组件··········#####//
+import EquipCard from "../common/EquipCard"; //装备卡片
+export default {
+  name: "EquipListPrimary",
+  props: {
+    /* 初级装备列表 */
+    equipList: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+  components: { EquipCard },
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.EquipListPrimary {
+  width: 33.33%;
+  height: 100%;
+}
+</style>
