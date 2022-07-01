2a3444b4f509cc8620c5d2982f992c785c4493c7
lengyibai <1329670984@qq.com>
2022-07-01 11:45:49 +0800

    增加高级装备列

---
+++ src/views/Equip/childComps/EquipList/childComps/EquipListSenior/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="EquipListSenior">
+    <EquipCard v-for="(item, index) in equipList" :equip="item" :key="index" />
+  </div>
+</template>
+<script>
+//#####··········子组件··········#####//
+import EquipCard from "../common/EquipCard"; //装备卡片
+export default {
+  name: "EquipListSenior",
+  props: {
+    /* 高级装备列表 */
+    equipList: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  data() {
+    return {};
+  },
+  components: { EquipCard },
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.EquipListSenior {
+  width: 33.33%;
+  height: 100%;
+}
+</style>
