e74772fbdcca7bffe32c6ac769a0fd98251cf36e
lengyibai <1329670984@qq.com>
2022-07-01 11:40:17 +0800

    增加装备描述盒子

---
+++ src/views/Equip/childComps/EquipDetail/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="EquipDetail"></div>
+</template>
+<script>
+export default {
+  name: "EquipDetail",
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.EquipDetail {
+  width: 25%;
+  height: 100%;
+  background-color: var(--black-25);
+}
+</style>
