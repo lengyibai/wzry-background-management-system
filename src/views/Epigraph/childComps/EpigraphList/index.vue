17f30c0ddfbddef43a45ac460d23b7c6828d52c9
lengyibai <1329670984@qq.com>
2022-07-02 10:30:03 +0800

    增加铭文列表组件

---
+++ src/views/Epigraph/childComps/EpigraphList/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition name="fade">
+    <div class="EpigraphList" v-if="flag">
+      <LybGridLayout gap="var(--gap-15)" :count="4" :eqhMultiple="0.5">
+        <EpigraphCard />
+        <EpigraphCard />
+        <EpigraphCard />
+        <EpigraphCard />
+        <EpigraphCard />
+        <EpigraphCard />
+      </LybGridLayout>
+    </div>
+  </transition>
+</template>
+<script>
+import EpigraphCard from "./childComps/EpigraphCard";
+export default {
+  name: "EpigraphList",
+  data() {
+    return { flag: false };
+  },
+  components: { EpigraphCard },
+  mounted() {
+    setTimeout(() => {
+      this.flag = true;
+    }, 250);
+  },
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.EpigraphList {
+  width: 100%;
+  height: 100%;
+  padding-top: var(--gap-15);
+}
+</style>
