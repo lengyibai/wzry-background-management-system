ae95f902652c218689c1636c6d12dccfa3c80473
lengyibai <1329670984@qq.com>
2022-07-02 10:30:38 +0800

    创建铭文等级筛选组件文件(未写功能及样式)

---
+++ src/views/Epigraph/childComps/EpigraphLevel/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="EpigraphLevel"></div>
+</template>
+<script>
+export default {
+  name: "EpigraphLevel",
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.EpigraphLevel {
+  width: 100%;
+  height: 100%;
+}
+</style>
