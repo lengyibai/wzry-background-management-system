4b77b4b31ea18569e6e2664ef299028058f0e489
lengyibai <1329670984@qq.com>
2022-07-03 22:03:05 +0800

    增加系统管理增、改、删的公共混入

---
+++ src/utils/mixins.js
@@ -0,0 +1,34 @@
+/* 系统管理卡片数据 */
+export const ManageCard = {
+  data() {
+    this.card = {
+      width: "200px",
+      height: "300px",
+      margin: "var(--gap-40)",
+    };
+    this.box = {
+      position: "relative",
+      width: "100%",
+      height: "100%",
+      flexWrap: "wrap",
+      overflow: "auto",
+    };
+    this.list = [
+      "英雄",
+      "皮肤",
+      "语音",
+      "技能信息",
+      "技能类型",
+      "技能效果",
+      "英雄故事",
+      "历史故事",
+      "英雄关系",
+      "关系类型",
+      "装备信息",
+      "装备类型",
+      "铭文信息",
+      "铭文类型",
+      "铭文效果",
+    ];
+  },
+};
