0df0a4e7ae1cbf019b3a21acbc5c74e339603ce3
lengyibai <1329670984@qq.com>
2022-07-03 22:02:25 +0800

    完成系统管理添加、修改、删除结构

---
+++ src/views/System/childViews/Delete/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="Delete flex" :style="box">
+    <K-ManageCard
+      v-for="(item, index) in list"
+      :title="item"
+      :key="index"
+      :style="card"
+      type="delete"
+    />
+  </div>
+</template>
+<script>
+//#####··········混入··········#####//
+// { 系统管理卡片数据 }
+import { ManageCard } from "@/utils/mixins.js";
+export default {
+  name: "Delete",
+  data() {
+    return {};
+  },
+  mixins: [ManageCard],
+  components: {},
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.Delete {
+}
+</style>
---
+++ src/views/System/childViews/Edit/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="Edit flex" :style="box">
+    <K-ManageCard
+      v-for="(item, index) in list"
+      :title="item"
+      :key="index"
+      :style="card"
+      type="edit"
+    />
+  </div>
+</template>
+<script>
+//#####··········混入··········#####//
+// { 系统管理卡片数据 }
+import { ManageCard } from "@/utils/mixins.js";
+export default {
+  name: "Edit",
+  data() {
+    return {};
+  },
+  mixins: [ManageCard],
+  components: {},
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.Edit {
+}
+</style>
