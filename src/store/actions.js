f5d4a2f0880a9b5eeb56d84fc22e7d8e0fcf0a75
lengyibai <1329670984@qq.com>
2022-07-05 23:37:05 +0800

    返回登录登出接口

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,7 +5,7 @@ export default {
   //#####··········登录··········#####//
   login(context, account) {
     // 请求登录接口
-    login(account).then(res => {
+    return login(account).then(res => {
       if (res.code === 200) {
         // 存储 token 到本地
         context.state.userStatus = true;
@@ -44,7 +44,7 @@ export default {
   },
   //#####··········退出登录··········#####//
   logout(context) {
-    logout(context.state.userInfo.id).then(() => {
+    return logout(context.state.userInfo.id).then(() => {
       context.commit("clearToken");
     });
   },
