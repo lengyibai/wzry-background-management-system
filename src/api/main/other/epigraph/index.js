2ac3ba72b29967fe9130288ce16c5fc1ab768f4b
lengyibai <1329670984@qq.com>
2022-07-06 12:11:05 +0800

    聚合铭文类型及效果

--- src/api/main/other/epigraph/index.js
+++ src/api/main/other/epigraph/index.js
@@ -1,10 +1,26 @@
+//#####··········资料聚合处理··········#####//
+//接口信息：{ 铭文效果 }
+import { getEpigraphEffect } from "@/api/main/tree/epigraphEffect";
+//接口信息：{ 铭文类型 }
+import { getEpigraphType } from "@/api/main/tree/epigraphType";
+
+//#####········获取铭文········#####//
 import { getEpigraphs } from "./transfer.js";
 
-//#####··········基本··········#####//
-//####········获取铭文········####//
 export const getEpigraph = data => {
   return new Promise(resolve => {
-    getEpigraphs(data).then(res => {
+    getEpigraphs(data).then(async res => {
+      for (let i = 0; i < res.data.length; i++) {
+        let _item = res.data[i];
+        _item.type = await getEpigraphType({ id: _item.type });
+        for (let j = 0; j < _item.effect.length; j++) {
+          _item.effect[j].type = await getEpigraphEffect({
+            id: _item.effect[j].type,
+          });
+        }
+      }
+
+      console.log(JSON.parse(JSON.stringify(res.data)));
       if (data) {
         resolve(res.data[0]);
       } else {
