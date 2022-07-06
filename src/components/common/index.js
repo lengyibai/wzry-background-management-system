587b58f9506b8793b40fee5ee51c848b670103ba
lengyibai <1329670984@qq.com>
2022-07-06 22:34:36 +0800

    遍历文件夹，自动注册全局组件

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,30 +1,13 @@
-//#####··········公共组件··········#####//
-import LybBgVideo from "./lyb-bg-video"; //视频背景
-import LybFlipBox from "./lyb-flip-box"; //卡片翻转
-import LybGridLayout from "./lyb-grid-layout"; //Grid经典布局
-import LybIcon from "./lyb-icon"; //图标
-import LybSvg from "./lyb-svg"; //图标
-import LybLoading from "./lyb-loading"; //加载动画
-import LybMask from "./lyb-mask"; //蒙版
-import LybMaskClose from "./lyb-mask-close"; //顶部关闭蒙版
-
-//使用组件
-const components = [
-  LybBgVideo,
-  LybFlipBox,
-  LybGridLayout,
-  LybIcon,
-  LybSvg,
-  LybLoading,
-  LybMask,
-  LybMaskClose,
-];
-export default {
-  //只会执行install
-  install(Vue) {
-    //注册组件
-    components.forEach(component => {
-      Vue.component(component.name, component);
-    });
-  },
-};
+import Vue from "vue";
+const requireComponents = require.context("@/components", true, /\.vue$/);
+requireComponents.keys().forEach(fileName => {
+  if (
+    fileName.includes("childComp") ||
+    fileName.includes("demo") ||
+    fileName.includes("SidebarItem")
+  )
+    return;
+  const reqCom = requireComponents(fileName).default;
+  const reqComName = reqCom.name;
+  Vue.component(reqComName, reqCom);
+});
