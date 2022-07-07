e8a31fb324afdff47fcca393b312c0f213d96520
lengyibai <1329670984@qq.com>
2022-07-08 01:23:21 +0800

    完成英雄设置头像

--- src/views/System/childViews/Add/childViews/AddHero/childComps/AddHeroHeadImg/index.vue
+++ src/views/System/childViews/Add/childViews/AddHero/childComps/AddHeroHeadImg/index.vue
@@ -1,28 +1,91 @@
 <template>
   <div class="AddHeroHeadImg">
-    <AddHeroCard title="头像">
-      <div class="headImg"></div>
-    </AddHeroCard>
+    <div class="set-headImg row">
+      <span class="title">英雄头像：</span>
+      <div class="headImg flex cursor-pointer" @click="select">
+        <img class="cursor-pointer" :src="link" alt="" v-if="link" />
+        <img class="add cursor-pointer" v-else src="./img/add.svg" alt="" />
+      </div>
+    </div>
+    <div class="set-name row">
+      <span class="title">英雄名：</span>
+      <span class="name cursor-pointer">{{ name || "点击设置" }}</span>
+    </div>
   </div>
 </template>
 <script>
+//#####··········SVG··········#####//
 export default {
   name: "AddHeroHeadImg",
+  props: {
+    link: {
+      type: String,
+      default: "",
+    },
+    name: {
+      type: String,
+      default: "",
+    },
+  },
   data() {
-    return {};
+    return {
+      hover: false,
+    };
   },
   components: {},
-  methods: {},
+  methods: {
+    select() {
+      this.$emit("select");
+    },
+  },
 };
 </script>
 <style scoped lang="less">
 .AddHeroHeadImg {
   width: 100%;
-  .headImg {
-    width: 150px;
-    height: 150px;
-    border-radius: 50%;
-    background-color: #fff;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  .row {
+    display: flex;
+    align-items: center;
+    .title {
+      font-size: var(--font-s-50);
+    }
+  }
+  .set-headImg {
+    .headImg {
+      position: relative;
+      width: 150px;
+      height: 150px;
+      border-radius: 50%;
+      transition: 0.25s;
+      background-color: transparent;
+      border: 5px solid #fff;
+      &:hover {
+        border-style: dashed;
+      }
+      &:active {
+        background-color: #555;
+      }
+      img {
+        object-fit: cover;
+        border-radius: 50%;
+        &:first-child {
+          width: 100%;
+          height: 100%;
+        }
+        &:last-child.add {
+          width: 75%;
+          height: 75%;
+        }
+      }
+    }
+  }
+  .set-name {
+    .name {
+      font-size: var(--font-s-50);
+    }
   }
 }
 </style>
--- src/views/System/childViews/Add/childViews/AddHero/index.vue
+++ src/views/System/childViews/Add/childViews/AddHero/index.vue
@@ -1,9 +1,15 @@
 <template>
   <div class="AddHero">
-    <LybMaskClose @close="hide" />
+    <!-- <LybMaskClose @close="hide" /> -->
+    <AddLink
+      v-model="show_AddLink"
+      :title="AddLink_title"
+      :placeholder="AddLink_placeholder"
+      @get-link="getLink"
+    />
     <transition name="fade">
       <div class="content" v-show="show">
-        <AddHeroHeadImg />
+        <AddHeroHeadImg :link="hero_data.headImg" @select="headImg" />
       </div>
     </transition>
   </div>
@@ -16,6 +22,14 @@ export default {
   data() {
     return {
       show: false,
+      //#####··········弹窗相关··········#####//
+      show_AddLink: false, //显示添加链接弹窗
+      AddLink_status: "", //当前谁在使用弹窗(字段名)
+      AddLink_title: "", //弹窗左上角标题
+      AddLink_placeholder: "", //弹窗输入框描述
+      hero_data: {
+        headImg: "",
+      },
     };
   },
   components: { AddHeroHeadImg },
@@ -31,6 +45,21 @@ export default {
       setTimeout(() => {
         this.$emit("input", false);
       }, 500);
+    },
+
+    //#####·········设置图片弹窗时间··········#####//
+    //####········获取链接········####//
+    getLink(link) {
+      this.hero_data.headImg = link;
+    },
+
+    //#####··········添加链接事件··········#####//
+    //####········头像········####//
+    headImg() {
+      this.show_AddLink = true;
+      this.AddLink_title = "设置英雄头像";
+      this.AddLink_placeholder = "请输入头像链接";
+      this.AddLink_status = "headImg";
     },
   },
 };
@@ -50,9 +79,6 @@ export default {
     width: 100%;
     height: 100%;
     color: #fff;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
   }
 }
 </style>
