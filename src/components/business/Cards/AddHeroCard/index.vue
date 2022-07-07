5969a3ad3fda911152f356e36799c23e0a522c28
lengyibai <1329670984@qq.com>
2022-07-07 14:46:09 +0800

    增加添加英雄内子组件占位卡片

---
+++ src/components/business/Cards/AddHeroCard/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="AddHeroCard">
+    <div class="title">{{ title }}</div>
+    <div class="box">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "AddHeroCard",
+  props: {
+    title: {
+      type: String,
+      default: "",
+    },
+  },
+  data() {
+    return {};
+  },
+  components: {},
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.AddHeroCard {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: var(--gap-35);
+  .title {
+    margin-bottom: var(--gap-20);
+    font-size: var(--font-s-30);
+  }
+}
+</style>
