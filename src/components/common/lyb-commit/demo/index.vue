79181f0719b646fcfe4396f2cb27643c0425e358
lengyibai <1329670984@qq.com>
2022-07-08 14:19:59 +0800

    增加提交按钮动效组件

---
+++ src/components/common/lyb-commit/demo/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="demoFull flex">
+    <h1>{{ text }}</h1>
+    <LybCommit
+      style="border: 1.25vw solid #fff"
+      @upload="upload"
+      :finish="finish"
+      size="25vw"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "demo",
+  data() {
+    return {
+      finish: false,
+      text: "待提交",
+    };
+  },
+  methods: {
+    upload() {
+      this.text = "提交中";
+      setTimeout(() => {
+        this.finish = true;
+        this.text = "提交成功";
+      }, 2000);
+    },
+  },
+};
+</script>
+<style scoped lang="less">
+.demoFull {
+  flex-direction: column;
+  h1 {
+    font-size: 5vw;
+    color: #fff;
+    margin-bottom: 1em;
+  }
+}
+</style>
---
+++ src/components/common/lyb-commit/index.vue
@@ -0,0 +1,119 @@
+<template>
+  <div
+    class="LybCommit"
+    @click="add"
+    :style="{
+      width: size,
+      height: size,
+    }"
+  >
+    <img
+      class="up"
+      v-show="show_up"
+      :class="{ move: status === 1 }"
+      src="./img/fabu.svg"
+    />
+    <transition name="fade">
+      <img
+        v-show="!show_up && status === 1 && !finish"
+        class="rotate"
+        src="./img/loading.svg"
+      />
+    </transition>
+    <transition name="bounce">
+      <img v-show="finish" src="./img/success.svg" />
+    </transition>
+  </div>
+</template>
+<script>
+export default {
+  name: "LybCommit",
+  props: {
+    size: {
+      type: String,
+      default: "500px",
+    },
+    finish: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      show_up: true,
+      status: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.status = 1;
+      setTimeout(() => {
+        this.show_up = false;
+        this.$emit("upload");
+      }, 200);
+    },
+  },
+};
+</script>
+<style scoped lang="less">
+.LybCommit {
+  position: relative;
+  display: flex;
+  border-radius: 50%;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  img {
+    width: 75%;
+    height: 75%;
+    &.up {
+      transition: all 0.5s;
+    }
+  }
+}
+
+.move {
+  transform: translateY(-200%);
+}
+
+.rotate {
+  animation: rotate 1s infinite linear;
+}
+@keyframes rotate {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
+
+.fade-leave-active,
+.fade-enter-active {
+  transition: all 0.25s;
+}
+
+.fade-leave-active {
+  position: absolute;
+}
+
+.bounce-enter,
+.bounce-leave-active {
+  opacity: 0;
+  transform: scale(0);
+}
+
+.bounce-leave-active,
+.bounce-enter-active {
+  transition: all 0.5s 0.2s;
+}
+
+.bounce-leave-active {
+  position: absolute;
+}
+</style>
