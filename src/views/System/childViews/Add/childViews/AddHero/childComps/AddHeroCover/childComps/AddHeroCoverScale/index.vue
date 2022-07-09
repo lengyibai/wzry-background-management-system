35d59c805cd998fdef1603b83e5a08098ef7c652
lengyibai <1329670984@qq.com>
2022-07-09 14:28:33 +0800

    增加英雄封面缩放量设置组件

---
+++ src/views/System/childViews/Add/childViews/AddHero/childComps/AddHeroCover/childComps/AddHeroCoverScale/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="AddHeroCoverOffset">
+    <LybSvg
+      :class="item.toLowerCase()"
+      class="a"
+      downColor="#aaa"
+      v-for="(item, index) in ['LEFT', 'RIGHT']"
+      @click.native="scale(item.toLowerCase())"
+      :key="index"
+      :svg="icon[item]"
+      color="#fff"
+      size="75px"
+    />
+  </div>
+</template>
+<script>
+import icon from "./svg";
+export default {
+  name: "AddHeroCoverOffset",
+  data() {
+    this.icon = icon;
+    return {};
+  },
+  components: {},
+  methods: {
+    scale(scale) {
+      this.$emit("scale", scale);
+    },
+  },
+};
+</script>
+<style scoped lang="less">
+.AddHeroCoverOffset {
+  position: relative;
+  width: 200px;
+  height: 300px;
+  .a {
+    position: absolute;
+  }
+  .top,
+  .bottom {
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  .bottom {
+    bottom: 0;
+  }
+  .left,
+  .right {
+    top: 50%;
+    transform: translateY(-50%);
+  }
+  .right {
+    right: 0;
+  }
+}
+</style>
