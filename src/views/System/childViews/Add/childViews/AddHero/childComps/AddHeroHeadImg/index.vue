6866394dd6d2046be3f5a7a2d0c0c2fa886635e0
lengyibai <1329670984@qq.com>
2022-07-09 14:31:54 +0800

    将选择头像抽离为组件

--- src/views/System/childViews/Add/childViews/AddHero/childComps/AddHeroHeadImg/index.vue
+++ src/views/System/childViews/Add/childViews/AddHero/childComps/AddHeroHeadImg/index.vue
@@ -1,43 +1,27 @@
 <template>
   <div class="AddHeroHeadImg">
-    <div class="set-headImg row">
+    <div class="row">
       <span class="title">英雄头像：</span>
-      <div class="headImg flex cursor-pointer" @click="select">
-        <img class="cursor-pointer" :src="link" alt="" v-if="link" />
-        <img class="add cursor-pointer" v-else src="./img/add.svg" alt="" />
-      </div>
+      <SelectImg
+        :src="hero_data.headImg"
+        keyword="headImg"
+        @select="setKeyValue"
+      />
     </div>
-    <div class="set-name row">
-      <span class="title">英雄名：</span>
-      <span class="name cursor-pointer">{{ name || "点击设置" }}</span>
+    <div class="row">
+      <span class="title">英雄海报：</span
+      ><SelectImg
+        :src="hero_data.poster"
+        keyword="poster"
+        @select="setKeyValue"
+      />
     </div>
   </div>
 </template>
 <script>
-//#####··········SVG··········#####//
 export default {
   name: "AddHeroHeadImg",
-  props: {
-    link: {
-      type: String,
-      default: "",
-    },
-    name: {
-      type: String,
-      default: "",
-    },
-  },
-  data() {
-    return {
-      hover: false,
-    };
-  },
-  components: {},
-  methods: {
-    select() {
-      this.$emit("select");
-    },
-  },
+  inject: ["hero_data", "setKeyValue"],
 };
 </script>
 <style scoped lang="less">
@@ -46,6 +30,7 @@ export default {
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+  margin-bottom: var(--gap-50);
   .row {
     display: flex;
     align-items: center;
@@ -53,37 +38,8 @@ export default {
       font-size: var(--font-s-50);
     }
   }
-  .set-headImg {
-    .headImg {
-      position: relative;
-      width: 150px;
-      height: 150px;
-      border-radius: 50%;
-      transition: 0.25s;
-      background-color: transparent;
-      border: 5px solid #fff;
-      &:hover {
-        border-style: dashed;
-      }
-      &:active {
-        background-color: #555;
-      }
-      img {
-        object-fit: cover;
-        border-radius: 50%;
-        &:first-child {
-          width: 100%;
-          height: 100%;
-        }
-        &:last-child.add {
-          width: 75%;
-          height: 75%;
-        }
-      }
-    }
-  }
-  .set-name {
-    .name {
+  .set-value {
+    .value {
       font-size: var(--font-s-50);
     }
   }
