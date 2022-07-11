ba260568ea99bee23ab9ac23ebdbee4fa6959269
lengyibai <1329670984@qq.com>
2022-07-11 12:55:18 +0800

    抽离选择图片组件

---
+++ src/views/System/childViews/Add/childViews/AddHero/childComps/AddHeroCover/childComps/AddHeroCoverImg/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="AddHeroCoverImg flex" @click="setKeyValue('cover')">
+    <LybSvg
+      :svg="icon.ADD"
+      color="var(--theme-color-eight)"
+      enter-color="var(--theme-color-four)"
+      size="75px"
+      v-show="!hero_data.cover"
+    />
+    <img
+      class="cover-img"
+      v-if="hero_data.cover"
+      :src="hero_data.cover"
+      :style="{
+        top: p.top + '%',
+        left: p.left + '%',
+        transform: p.transform,
+      }"
+    />
+  </div>
+</template>
+<script>
+//#####··········图标··········#####//
+import icon from "./svg";
+export default {
+  name: "AddHeroCoverImg",
+  props: {
+    /* 偏移量及缩放量 */
+    p: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  data() {
+    this.icon = icon;
+    return {};
+  },
+  inject: ["hero_data", "setKeyValue"],
+  methods: {},
+};
+</script>
+<style scoped lang="less">
+.AddHeroCoverImg {
+  position: relative;
+  width: 200px;
+  height: 300px;
+  overflow: hidden;
+  border: 5px solid var(--theme-color-eight);
+  transition: all 0.25s;
+  border-image: linear-gradient(315deg, #3774b4 0%, #73b1d5 100%) 1 1 !important;
+  &:hover {
+    border-image: linear-gradient(315deg, #5989a5 0%, #295686 100%) 1 1 !important;
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+  .cover-img {
+    position: absolute;
+    min-width: 100%;
+    max-height: 100%;
+    pointer-events: none;
+  }
+}
+</style>
--- src/views/System/childViews/Add/childViews/AddHero/childComps/AddHeroCover/index.vue
+++ src/views/System/childViews/Add/childViews/AddHero/childComps/AddHeroCover/index.vue
@@ -1,43 +1,27 @@
 <template>
   <div class="AddHeroCover">
-    <div class="row">
-      <span class="title text-gradient-one">英雄封面：</span>
-      <div class="cover flex" @click="setKeyValue('cover')">
-        <LybSvg
-          :svg="icon.ADD"
-          color="var(--theme-color-three)"
-          enter-color="var(--theme-color-seven)"
-          size="75px"
-          v-show="!hero_data.cover"
-        />
-        <img
-          class="cover-img"
-          v-if="hero_data.cover"
-          :src="hero_data.cover"
-          :style="{
-            top: p.top + '%',
-            left: p.left + '%',
-            transform: p.transform,
-          }"
-        />
-      </div>
-    </div>
+    <span class="title text-gradient-one">英雄封面：</span>
+
+    <!--//%%%%%··········选择封面··········%%%%%//-->
+    <AddHeroCoverImg :p="p" />
+
+    <!--//%%%%%··········偏移量··········%%%%%//-->
     <AddHeroCoverOffset @direction="direction" />
+
+    <!--//%%%%%··········缩放量··········%%%%%//-->
     <AddHeroCoverScale @scale="scale" />
   </div>
 </template>
 <script>
-//#####··········图标··········#####//
-import icon from "./svg";
 //#####··········子组件··········#####//
+import AddHeroCoverImg from "./childComps/AddHeroCoverImg";
 import AddHeroCoverOffset from "./childComps/AddHeroCoverOffset"; //调整偏移量
 import AddHeroCoverScale from "./childComps/AddHeroCoverScale"; //调整缩放量
 export default {
   name: "AddHeroCover",
   data() {
-    this.icon = icon;
     return {
-      size: 1,
+      size: 1, //缩放量
       p: {
         top: 0,
         left: 0,
@@ -45,13 +29,14 @@ export default {
       },
     };
   },
-  components: { AddHeroCoverOffset, AddHeroCoverScale },
-  inject: ["hero_data", "setKeyValue", "setKeyV"],
+  components: { AddHeroCoverImg, AddHeroCoverOffset, AddHeroCoverScale },
+  inject: ["hero_data", "setKeyV"],
   methods: {
     //#####··········发射事件··········#####//
     emit() {
       this.setKeyV("offset", this.p);
     },
+
     //#####··········设置偏移量··········#####//
     direction(d) {
       if (!this.hero_data.cover) {
@@ -93,33 +78,8 @@ export default {
   justify-content: space-evenly;
   align-items: center;
   margin-bottom: var(--gap-35);
-  .row {
-    display: flex;
-    align-items: center;
-    .title {
-      font-size: var(--font-s-30);
-    }
-    .cover {
-      position: relative;
-      width: 200px;
-      height: 300px;
-      overflow: hidden;
-      border: 5px solid var(--theme-color-eight);
-      transition: all 0.25s;
-      border-image: linear-gradient(315deg, #3774b4 0%, #73b1d5 100%) 1 1 !important;
-      &:hover {
-        border-image: linear-gradient(315deg, #5989a5 0%, #295686 100%) 1 1 !important;
-      }
-      &:active {
-        transform: scale(0.95);
-      }
-      .cover-img {
-        position: absolute;
-        min-width: 100%;
-        max-height: 100%;
-        pointer-events: none;
-      }
-    }
+  .title {
+    font-size: var(--font-s-30);
   }
 }
 </style>
