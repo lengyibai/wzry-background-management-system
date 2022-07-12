dd0a307d7ac701b3169297d9e02d9efe5bf51115
lengyibai <1329670984@qq.com>
2022-07-12 13:49:17 +0800

    增加取消按钮组件

---
+++ src/components/common/lyb-cancel-btn/demo/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="demoFull flex">
+    <h1>取消</h1>
+    <LybCancelBtn
+      style="border: 1.25vw solid #fff"
+      @close="close"
+      size="25vw"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "demo",
+  methods: {
+    close() {
+      console.warn("触发关闭");
+    },
+  },
+};
+</script>
+<style scoped lang="less">
+.demoFull {
+  flex-direction: column;
+  h1 {
+    font-size: 5vw;
+    color: #fff;
+    margin-bottom: 1em;
+  }
+}
+</style>
---
+++ src/components/common/lyb-cancel-btn/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <div
+    class="LybCancelBtn"
+    @click="close"
+    :style="{
+      width: size,
+      height: size,
+    }"
+  >
+    <img
+      class="close"
+      v-show="show_up"
+      :class="{ move: status === 1 }"
+      src="./img/close.svg"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "LybCancelBtn",
+  props: {
+    size: {
+      type: String,
+      default: "500px",
+    },
+  },
+  data() {
+    return {
+      show_up: true,
+      status: 0,
+    };
+  },
+  methods: {
+    close() {
+      this.status = 1;
+      setTimeout(() => {
+        this.$emit("close");
+      }, 350);
+    },
+  },
+};
+</script>
+<style scoped lang="less">
+.LybCancelBtn {
+  position: relative;
+  display: flex;
+  border-radius: 50%;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  img {
+    width: 75%;
+    height: 75%;
+    transition: all 0.5s;
+  }
+}
+
+.move {
+  transform: scale(0);
+}
+</style>
