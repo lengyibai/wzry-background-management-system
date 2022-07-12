ce7c166de42c4db939d77b553e1197735aae4e64
lengyibai <1329670984@qq.com>
2022-07-12 13:50:06 +0800

    增加更新按钮

---
+++ src/components/common/lyb-update-btn/demo/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="demoFull flex">
+    <h1>{{ text }}</h1>
+    <LybUpdateBtn
+      style="border: 1.25vw solid #fff"
+      @update="update"
+      :finish="finish"
+      size="25vw"
+    />
+  </div>
+</template>
+<script>
+export default {
+  name: "demo",
+  data() {
+    return {
+      finish: false,
+      text: "待更新",
+    };
+  },
+  methods: {
+    update() {
+      this.text = "更新中";
+      setTimeout(() => {
+        this.finish = true;
+        this.text = "更新成功";
+      }, 2000);
+    },
+  },
+};
+</script>
+<style scoped lang="less">
+.demoFull {
+  flex-direction: column;
+  h1 {
+    font-size: 5vw;
+    color: #fff;
+    margin-bottom: 1em;
+  }
+}
+</style>
