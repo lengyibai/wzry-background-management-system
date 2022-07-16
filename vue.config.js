54f51ca92cb4ecdb8a57618db754847cbaacf126
lengyibai <1329670984@qq.com>
2022-07-16 13:06:17 +0800

    全局使用less变量及函数等功能

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const { defineConfig } = require("@vue/cli-service");
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 
@@ -5,6 +6,12 @@ module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     plugins: [new NodePolyfillPlugin()],
+  },
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [path.join(__dirname, "./src/styles/index.less")],
+    },
   },
   publicPath: "./",
 });
