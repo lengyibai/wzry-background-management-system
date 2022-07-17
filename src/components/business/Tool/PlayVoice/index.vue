52bc354b245b8924f78af4721e34c1d28abbe35c
lengyibai <1329670984@qq.com>
2022-07-17 22:21:26 +0800

    增加播放音频组件

---
+++ src/components/business/Tool/PlayVoice/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <audio @ended="ended" :src="link" ref="voice" hidden="true"></audio>
+  </div>
+</template>
+<script>
+export default {
+  name: "PlayVoice",
+  props: {
+    link: {
+      type: String,
+      default: "",
+    },
+  },
+  mounted() {
+    const audio = this.$refs.voice;
+    audio.addEventListener("canplay", () => {
+      audio.play();
+    });
+  },
+  methods: {
+    ended() {
+      this.$emit("ended");
+    },
+  },
+};
+</script>
