30f18e8f286de20254330f3d5f5af0ba42de767b
lengyibai <1329670984@qq.com>
2022-07-22 12:50:17 +0800

    增加发布装备页

---
+++ src/views/System/childViews/Add/childViews/AddEpigraph/index.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="AddEquip">
+    <transition name="fade">
+      <div
+        class="content"
+        ref="content"
+        v-if="show"
+        is="transition-group"
+        name="fade"
+      ></div>
+    </transition>
+
+    <!--//%%%%%··········发布按钮··········%%%%%//-->
+    <LybCommitBtn
+      class="LybCommitBtn"
+      size="50px"
+      @upload="add"
+      :finish="add_finish"
+      title="发布"
+    />
+
+    <!--//%%%%%··········取消发布··········%%%%%//-->
+    <LybCancelBtn class="LybCancelBtn" size="50px" @close="hide" title="取消" />
+  </div>
+</template>
+<script>
+//#####··········混入··········#####//
+// { 发布及隐藏自身 }
+import { addHide } from "@/utils/mixins.js";
+export default {
+  name: "AddEquip",
+  data() {
+    return {};
+  },
+  mixins: [addHide],
+  created() {
+    setTimeout(async () => {
+      this.$lybLoad.show("正在加载");
+      this.$lybLoad.close().then(() => {
+        this.show = true;
+      });
+    }, 1000);
+  },
+  methods: {
+    //#####··········发布··········#####//
+    add() {
+      setTimeout(() => {
+        this.add_finish = true;
+        setTimeout(() => {
+          this.hide();
+        }, 250);
+      }, 250);
+    },
+  },
+};
+</script>
+<style scoped lang="less">
+.AddEquip {
+  .view_add();
+  .content {
+  }
+}
+</style>
